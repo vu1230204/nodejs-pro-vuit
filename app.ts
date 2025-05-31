1444bf5fcd8799835be4b1732021a0c1e7688343
Vu Nguyen <vu.nguyen@zimaw.com>
2025-05-31 21:57:45 +0700

    video 23

---
+++ app.ts
@@ -0,0 +1,15 @@
+// const express = require("express");
+import express from "express";
+const app = express();
+const PORT = 8080;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.get("/tamvuit", (req, res) => {
+  res.send("Hello vu");
+});
+
+app.listen(8080, () => {
+  console.log(`My App is running on port : ${PORT}`);
+});
